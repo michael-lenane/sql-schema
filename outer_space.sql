8ab61337d4813ca9ee4e01a2d60291b067f17b58
Michael Lenane <michael.j.lenane@gmail.com>
2022-07-04 11:55:04 -0400

    updated schema

--- outer_space.sql
+++ outer_space.sql
@@ -11,10 +11,15 @@ CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy TEXT NOT NULL REFERENCES galaxies,
+);
+
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  galaxy_name TEXT NOT NULL
 );
 
 INSERT INTO planets
